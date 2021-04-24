c9b0c7615d8a69a502eed1f490d06f7f81cdabf9
Shauna Myers <shaunamyers26@gmail.com>
2021-04-24 13:25:02 -0400

    Move data model logic to Game and Player class

--- Game.js
+++ Game.js
@@ -1,19 +1,69 @@
 class Game {
-  constructor(mode, player1, player2) {
-    this.mode = mode;
+  constructor(player1, player2) {
     this.player1 = player1;
     this.player2 = player2;
-    console.log(this);
+    this.mode = '';
   }
-  determineWinner() {
-    var winner;
-  if (this.player1.token > this.player2.token) {
-    winner = player1;
-  } else if (this.player2.token > this.player1.token) {
-    winner = player2;
-  } else {
-    this.player.updateWins();
+
+// player instances here
+
+  evaluateWinner(gameChoice) {
+    var token1 = this.player1.token;
+    var token2 = this.player2.token;
+    if (gameChoice === 'classic') {
+      this.mode = gameChoice;
+      this.classicWinner(token1, token2)
+    }  else {
+      this.mode = gameChoice;
+      this.difficultWinner(token1, token2);
+    }
+  }
+
+  classicWinner(token1, token2) {
+    // logic for what beats what
+    // Would a switch statement be the best thing?
+    // invoke player1.updateWins or player2.updateWins
+
+    // How do I get what is returned from this?
+    // Can I save all these ternaries in a variable?
+    // ***ORRR I think this is returning anything that is on the other side of the "?'***
+      // If the condition evaluates to true
+      // can I just make an if statment in updateWins that says if (classicWinner === player1)
+      // else if (classicWinner === player2)
+      // I can then have another code block for "Draw"
+      // This will eventually invoke a function that would insert innerText that states it's a draw
+
+      // if else
+    if (token1 === 'rock' && token2 === 'scissors' || token1 === 'paper' && token2 === 'rock') { 
+      return player1;
+    }
+    (token1 === 'paper' && token2 === 'rock') ? player1 :
+    (token1 === 'scissors' && token2 === "paper") ? player1 :
+
+    (token2 === 'rock' && token2 === 'scissors') ? player2 :
+    (token2 === 'paper' && token2 === 'rock') ? player2 :
+    (token2 === 'scissors' && token2 === "paper") ? player2 : 'Draw' // Maybe invoke difficultWinner here?
+    // Could I simply call one function from evaluateWinner
+    // THEN in the else statement on line 39, invoke difficultWinner
+      // DifficultWinner would then include the other conditions
+      // Follows the DRY principle...?
+
 
   }
+
+  difficultWinner(token1, token2) {
+
+    // This logic is probably too much for ternary...think we're supposed to keep them to one line
+
+    // Can you do this???
+    if (token1 === 'rock' && (token2 === 'scissors' || token2 === 'potted plant')
+
+
+    // logic for what beats what in the difficult version
+    // invoke player1.updateWins or player2.updateWins
+  }
+
+  resetBoard() {
+
   }
 }
--- main.js
+++ main.js
@@ -29,9 +29,22 @@ function changeGameMode(event) {
 
 
   function selectFighter1(event) {
+
+    // I want to get this id and use it as the variable name (above in my query selectors) to toggle views
+    // Can I do this?
+    // Can't get it to work right now
+    console.log(event.target.closest('section').id); // I get the correct id name...classicFighters or difficultFighters
+    var battleModeView = event.target.closest('section').id;
+    console.log(typeof battleModeView); // A string...this is why I can't use this as the variable name?
+    // toggleHidden(`${whatIsReturned}`);
+    // toggleHidden(`${event.target.closest('section').id}`)
+
+
     var tokenChoice1 = event.target.closest('button').id;
     var gameChoice = event.target.closest('div').id;
+    console.log('1', gameChoice);
     var player1 = new Player('human', tokenChoice1)
+
     // Another function invoked
     // invoke takeTurn method...do I need to declare player2 here? or in takeTurn?
     selectFighter2(player1, gameChoice)
@@ -39,8 +52,13 @@ function changeGameMode(event) {
 
   function selectFighter2(player1, gameChoice) {
     var tokens = ['rock', 'paper', 'scissors', 'plant', 'llama'];
+
+    // Is this what the takeTurn method is supposed to do?
+      // Generate a random token inside takeTurn?
+      // I don't believe I should instantiate a new instance of player there, though
     var tokenChoice2 = tokens[getRandomIndex(tokens)];
     var player2 = new Player('computer', tokenChoice2);
+
     currentGame = new Game(player1, player2);
     currentGame.evaluateWinner(gameChoice);
   }
