ea55e29b4ec6e5609ffa1385187baf6ce5cd9c67
Shauna Myers <shaunamyers26@gmail.com>
2021-05-09 12:01:44 -0400

    Change saveGameDetails function for clarity

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ window.onload = function() {
 }
 
 
-gameSelection.addEventListener('click', function(event){
+gameSelection.addEventListener('click', function(event) {
   changeGameView(event);
   startNewGame(event);
 });
@@ -52,7 +52,9 @@ function changeGameView(event) {
 
 
 function startNewGame(event) {
-  currentGame.saveGameDetails(event);
+  var sectionName = event.target.closest('section').id;
+  var buttonName = event.target.closest('button').id;
+  currentGame.saveGameDetails(sectionName, buttonName);
   displayChangeGameBtn();
 }
 
@@ -63,8 +65,10 @@ function displayChangeGameBtn() {
 
 
 function selectFighterView(event) {
+  var sectionName = event.target.closest('section').id;
+  var buttonName = event.target.closest('button').id;
   changeHiddenViews(eval(`${event.target.closest('div').id}Fighters`), battleMode);
-  currentGame.saveGameDetails(event);
+  currentGame.saveGameDetails(sectionName, buttonName);
   displayResetScoreBtn();
 }
 
@@ -168,6 +172,6 @@ function resetPlayersScore() {
 
 function changeHiddenViews() {
   for (var i = 0; i < arguments.length; i++) {
-      arguments[i].classList.toggle('hidden');
+    arguments[i].classList.toggle('hidden');
   }
 }
